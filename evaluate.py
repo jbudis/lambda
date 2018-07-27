ce3cbaedff1be93b66c70629baf85fed84b5ecb7
Jaroslav Budis <jaroslav.budis@gmail.com>
2018-07-27 14:26:04 +0200

    Loading of count files

--- evaluate.py
+++ evaluate.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from argparse import ArgumentParser
-# import pandas as pd
+import pandas as pd
 # import numpy as np
 # import matplotlib.pyplot as plt
 # from scipy import stats
@@ -20,10 +20,10 @@ from argparse import ArgumentParser
 parser = ArgumentParser(description='Calculates standard and FL-based score for diagnosed sample'
                                     'and predicts diagnosis, i.e. positive, false positive, uninformative, negative')
 
-parser.add_argument('counts', help='TSV file with the number or reads '
+parser.add_argument('counts', help='TSV file with the corrected number or reads '
                                    'for each assumed fragment length (50-220, organised in columns) '
                                    'and autosomes (chr1..chr22, organised in rows)')
 
 args = parser.parse_args()
 
-print(args)
+counts = pd.read_csv(args.counts, sep='\t')
