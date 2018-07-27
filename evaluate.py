a834621dc157be61df0613877715ae09a5a65bb3
Jaroslav Budis <jaroslav.budis@gmail.com>
2018-07-27 14:33:06 +0200

    Control for number of columns and rows in input count file

--- evaluate.py
+++ evaluate.py
@@ -26,4 +26,17 @@ parser.add_argument('counts', help='TSV file with the corrected number or reads 
 
 args = parser.parse_args()
 
-counts = pd.read_csv(args.counts, sep='\t')
+AUTOSOMES = ['chr{}'.format(i) for i in range(22)]
+MIN_FL, MAX_FL = 50, 220
+FLS = list(range(MIN_FL, MAX_FL))
+
+counts = pd.read_csv(args.counts, sep='\t', header=0, index_col=0).values
+n_rows, n_cols = counts.shape
+
+assert n_rows == len(AUTOSOMES), 'Number or rows of input count file ' \
+                                 'does not correspond to {} autosomes'.format(len(AUTOSOMES))
+
+assert n_cols == len(FLS), 'Number of columns does not correspond to ' \
+                           'number of used fragment lengths, from {} to {}, ' \
+                           'i.e. {} columns'.format(MIN_FL, MAX_FL, MAX_FL - MIN_FL)
+
